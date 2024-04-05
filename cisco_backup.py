c73492af145428e5a38271f895cc88326369d73a
0988118277z <0988118277z@gmail.com>
2024-04-05 15:26:07 +0800

    backup v1

---
+++ cisco_backup.py
@@ -0,0 +1,34 @@
+import paramiko, time, getpass
+from datatime import datetime
+
+def auto_cisco_backup(ip, port, user, passwd, enablepasswd):
+    print('backup start')
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip=ip, port=port, username=user, password=passed)
+    
+    with open("backup_cmd.csv", 'r', encoding='utf-8') as f:
+        cmds = f.read().split('\n')
+        
+    chan = client.invoke_shell()
+    time.sleep(.5)
+    chan.send('en\n')
+    chan.send(enablepasswd+'\n')
+    time.sleep(1)
+    for cmd in cmds:
+        chan.send(cmd+'\n')
+        time.sleep(2)
+        
+    outputs = chan.recv(99999)
+    with open(f"{ip}_{datetime.now().strftime('%Y%m%d')}.txt", 'a+') as f:
+        f.write(str(outputs.decode('utf-8')))
+    
+    print('backup success')
+    client.close()
+    
+ip = input('ip = ')
+user = input('user = ')
+passwd = getpass.getpass('passwd = ')
+enablepasswd = getpass.getpass('enablepasswd = ')
+
+auto_cisco_backup(ip=ip, port=22, user=user, passwd=passwd, enablepasswd=enablepasswd)
